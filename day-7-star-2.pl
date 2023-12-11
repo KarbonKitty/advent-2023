08c1eba4716d5f2c49f987a8e757f68dc801847a
KarbonKitty <308lapua.magnum@gmail.com>
2023-12-11 21:00:56 +0100

    Day 7, fix star 1 and add star 2

--- day-7-star-2.pl
+++ day-7-star-2.pl
@@ -4,7 +4,7 @@ use 5.036;
 
 chomp(my @lines = <>);
 
-my %labelValues = ( 'A' => 13, 'K' => 12, 'Q' => 11, 'J' => 10, 'T' => 9, '9' => 8, '8' => 7, '7' => 6, '6' => 5, '5' => 4, '4' => 3, '3' => 2, '2' => 1 );
+my %labelValues = ( 'A' => 13, 'K' => 12, 'Q' => 11, 'T' => 9, '9' => 8, '8' => 7, '7' => 6, '6' => 5, '5' => 4, '4' => 3, '3' => 2, '2' => 1, 'J' => 0 );
 my %valuedHands;
 
 for my $line (@lines) {
@@ -17,9 +17,8 @@ for my $line (@lines) {
 my @sortedHandValues = sort { $a <=> $b } keys %valuedHands;
 
 my $sum = 0;
-my $i = 0;
 
-for $i (1..($#sortedHandValues + 1)) {
+for my $i (1..($#sortedHandValues + 1)) {
     my $bet = $valuedHands{$sortedHandValues[$i - 1]};
     $sum += ($bet * $i);
 }
@@ -30,23 +29,35 @@ sub handValue($hand) {
     my $value = 0;
     my @cards = split(//, $hand);
     my %cards;
+    my $jokerCount = 0;
     foreach my $card (@cards) {
-        $cards{$card} += 1;
+        if ($card eq 'J') {
+            $jokerCount += 1;
+        } else {
+            $cards{$card} += 1;
+        }
     }
 
     my @cardCounts = sort { $b <=> $a } values %cards;
 
-    if ($cardCounts[0] == 5) {
+    my $x = 0;
+    if ($#cardCounts >= 0) {
+        $x = $cardCounts[0];
+    }
+
+    my $biggestGroup = $x + $jokerCount;
+
+    if ($biggestGroup == 5) {
         $value = (6 << 20);
-    } elsif ($cardCounts[0] == 4) {
+    } elsif ($biggestGroup == 4) {
         $value = (5 << 20);
-    } elsif ($cardCounts[0] == 3) {
+    } elsif ($biggestGroup == 3) {
         if ($cardCounts[1] == 2) {
             $value = (4 << 20);
         } else {
             $value = (3 << 20);
         }
-    } elsif ($cardCounts[0] == 2) {
+    } elsif ($biggestGroup == 2) {
         if ($cardCounts[1] == 2) {
             $value = (2 << 20);
         } else {
@@ -62,6 +73,8 @@ sub handValue($hand) {
         my $shiftValue = (4 - $i) * 4;
         $value |= ($cardValue << $shiftValue);
     }
+
+    # say $value;
 
     return $value;
 }
